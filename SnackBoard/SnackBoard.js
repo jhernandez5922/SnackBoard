8940c677a7391b658aaa917d48cf37642f82d424
Jason Hernandez <j.hernandez5922@gmail.com>
2016-02-27 09:40:30 -0800

    Some progress, added navbar and dummy data for the tabs, still pretty early in design so stuff will change

--- SnackBoard/SnackBoard.js
+++ SnackBoard/SnackBoard.js
@@ -1,21 +1,91 @@
+Users = new Mongo.Collection('users');
+
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    
+    //Helps set up navbar
+    Template.registerHelper('groupRoutes', function () {
+    FlowRouter.watchPathChange()
+    var groupName = FlowRouter.current().route.group.name
+    return _.filter(FlowRouter._routes, function (route) {
+        return route.group.name === groupName
+    })
+    })
+    // User data 
+    var user_data = [
+        {
+            user:"Jason",
+            amount:"$10.00"
+        },
+        {
+            user: "Daniel",
+            amount: "$0.01"
+        },
+        {
+            user:"Jason",
+            amount:"$10.00"
+        },
+        {
+            user: "Daniel",
+            amount: "$0.01"
+        },{
+            user:"Jason",
+            amount:"$10.00"
+        },
+        {
+            user: "Daniel",
+            amount: "$0.01"
+        },{
+            user:"Jason",
+            amount:"$10.00"
+        },
+        {
+            user: "Daniel",
+            amount: "$0.01"
+        },{
+            user:"Jason",
+            amount:"$10.00"
+        },
+        {
+            user: "Daniel",
+            amount: "$0.01"
+        },
+    ];
+    //Menu data
+    var menu_items = [
+        {
+            name: "Muffin",
+            cost: "$1.00",
+            stock: "5"
+        },
+        {
+            name: "Dr. Pepper",
+            cost: "$0.75",
+            stock: "2"
+        },
+        {
+            name: "Cheetos",
+            cost: "$0.50",
+            stock: "8"
+        },
+        {
+            name: "Ice Cream Pop",
+            cost: "$1.25",
+            stock: "1"
+        },
+        {
+            name: "Chicken Bake",
+            cost: "$3.25",
+            stock: "3"
+        }
+    ]
+    //Port data to user list
+    Template.user_list.helpers({users:user_data});
+    //Port data to menu list
+    Template.menu_items.helpers({item:menu_items});
+    
 }
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
---
+++ SnackBoard/routes.js
@@ -0,0 +1,24 @@
+
+var publicRoutes = FlowRouter.group({
+  name: 'public',
+  triggersEnter: []
+})
+
+publicRoutes.route('/', {
+  name: 'Snack Bar',
+  triggersEnter: [],
+  action: function (params, queryParams) {
+    BlazeLayout.render("ApplicationLayout",
+        { header: "welcome", main: "menu_items"})
+  },
+  triggersExit: []
+})
+publicRoutes.route('/users', {
+  name: 'Users',
+  triggersEnter: [],
+  action: function (params, queryParams) {
+    BlazeLayout.render('ApplicationLayout', 
+    {header: 'welcome', main:"user_list"})
+  },
+  triggersExit: []
+})
