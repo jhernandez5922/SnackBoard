f802f85f49fc0a6dd2e084cbe3638d0d43ad8225
Jason Hernandez <j.hernandez5922@gmail.com>
2016-06-30 17:08:01 -0700

    finished cart

--- SnackBoard/client/Cart.js
+++ SnackBoard/client/Cart.js
@@ -16,21 +16,31 @@ Template.Cart.events({
     var cost = 0;
     var cart = Session.get('cart');
     console.log(cart);
-    console.log($(event.target));
+    console.log($(event.target).val());
+    //if value 0 or less
     if ($(event.target).val() > 0) {
       for (var i = 0; i < cart.length; i++) {
         if (cart[i].name == this.name)
           cart[i].stock = $(event.target).val();
         cost += cart[i].cost * cart[i].stock;
       }
+      Session.set('cost', cost);
     } else {
-      var index = cart.indexOf(this);
-      if (index != -1) {
-        cart.splice(index, 1);
-        cost -= this.cost * this.stock;
-      }
+      $(event.target).val(this.stock);
     }
     console.log(cost);
+    Session.set('cart', cart);
+  },
+  'click .right-side': function(event) {
+    var cart = Session.get('cart');
+    var cost = Session.get('cost');
+    console.log(cart);
+    for (var i = 0; i < cart.length; i++) {
+      if (cart[i].name == this.name) {
+          cost -= cart[i].cost * cart[i].stock;
+          cart.splice(i, 1);
+      }
+    }
     Session.set('cost', cost);
     Session.set('cart', cart);
   }
